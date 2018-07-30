86abba051af54d5b9b1322fac5499e9409713167
Michael Zamora <36097056+mikezamora1998@users.noreply.github.com>
2018-07-30 11:38:58 -0600

    added layout.js and layout.css

---
+++ layout.js
@@ -0,0 +1,29 @@
+/*
+Usage: <script src="https://mikezamora1998.github.io/csup-scripts/layout.js" async></script>
+Present on pages: https://www.csupueblo.edu/admissions/apply-now.html
+*/
+function changeImage(ele) {
+    var id = ele.id;
+    var checked = (ele.src.includes("checked"))?true:false;
+    var a;
+    if (checked) {
+      checked = false
+      a = "/render/file.act?path=admissions/_img/logo-check-list.png";
+    } else {
+      checked = true
+      a = "/render/file.act?path=admissions/_img/logo-check-list-checked.png";
+    }
+    document.getElementById(id).src = a;
+  }
+
+  function HideTextBox(ele){
+     var id = ele.id;
+    
+     if(document.getElementById(id+"Text").style.display == 'none'){
+         document.getElementById(id+"Text").style.display = '';
+         document.getElementById(id +"Text").style.backgroundColor = '#f4f4f4';
+     }else{
+         document.getElementById(id +"Text").style.display = 'none';
+         document.getElementById(id +"Text").style.backgroundColor = '#ffffff';
+     }
+}
